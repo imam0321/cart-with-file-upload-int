2d180ccbbdef9869fd26d1c726c62259c7d64c3e
imam0321 <imam.hossain0321@gmail.com>
2024-11-06 20:49:51 +0600

    prjects setup

---
+++ cart-with-file-upload-int-client/src/page/Home/Home.jsx
@@ -0,0 +1,12 @@
+import Incomplete from "../Incomplete/Incomplete";
+
+
+const Home = () => {
+  return (
+    <div>
+      <Incomplete/>
+    </div>
+  );
+};
+
+export default Home;
---
+++ cart-with-file-upload-int-client/src/page/Incomplete/Incomplete.jsx
@@ -0,0 +1,18 @@
+const Incomplete = () => {
+  return (
+    <div>
+      {/* header */}
+      <div className="w-[25%] flex justify-between items-center">
+        <div className="flex">
+          <span className="bg-red-600 px-3 rounded-s-3xl me-1"></span>
+          <p className="font-semibold">Incomplete</p>
+        </div>
+        <div className="px-1 rounded-sm bg-slate-300">0</div>
+      </div>
+      {/* cart */}
+      
+    </div>
+  );
+};
+
+export default Incomplete;
